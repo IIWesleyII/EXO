bb505b7228f8df5f8202236429bb0d07f3b9091f
leaf-turner <wesley.shaw.475@my.csun.edu>
2022-07-01 12:08:08 -0700

    finished generate exo df function

--- scrape_exo_data.py
+++ scrape_exo_data.py
@@ -9,13 +9,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
-
-
-
 def get_title(driver):
     return driver.title
 
-def get_table(driver):
+# returns the generated data frame
+def generate_exoplanet_df(driver):
     planet_name = []
     dist_from_earth = []
     planet_mass = []
@@ -28,27 +26,34 @@ def get_table(driver):
     page = 1
 
     while page <= int(max_page):
+        driver.refresh()
+        
         try:
             table = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((
                 By.CLASS_NAME, "exoplanet")))
-           
-            for row in table:
-                planet_name.append( row.find_element(By.CLASS_NAME, "display_name").text )
-                dist_from_earth.append( row.find_element(By.CLASS_NAME, "st_dist").text )
-                planet_mass.append( row.find_element(By.CLASS_NAME, "mass_display").text )
-                stellar_magnitude.append( row.find_element(By.CLASS_NAME, "st_optmag").text )
-                discovery_date.append( row.find_element(By.CLASS_NAME, "discovery_date").text )
-
-            page += 1
-            print(f'page:{page}')
-            WebDriverWait(driver,10).until(EC.element_to_be_clickable((
-                By.CSS_SELECTOR,"next"))).click()
-            
         except TimeoutException:
-            print('web driver failure')
+            print('WEB DRIVER FAILURE')
             driver.quit() 
+           
+        for row in table:
+            planet_name.append(row.find_element(By.CLASS_NAME, "display_name").text)
+            dist_from_earth.append(row.find_element(By.CLASS_NAME, "st_dist").text)
+            planet_mass.append(row.find_element(By.CLASS_NAME, "mass_display").text)
+            stellar_magnitude.append(row.find_element(By.CLASS_NAME, "st_optmag").text)
+            discovery_date.append(row.find_element(By.CLASS_NAME, "discovery_date").text)
 
-    print(planet_name)
+        
+        next_page = WebDriverWait(driver,10).until(EC.element_to_be_clickable((
+            By.XPATH, "//*[@id='primary_column']/footer/div/div/div/nav/span[2]/a")))
+
+        page += 1
+
+
+
+    df = pd.DataFrame({"Planet Name" : planet_name, "Distance From Earth" : dist_from_earth, "Planet Mass" : planet_mass,
+    "Stellar Magnitude" : stellar_magnitude, "Discovery Date" : discovery_date})
+
+    return df
 
 x=Service('C:\Program Files (x86)\chromedriver.exe')
 driver = webdriver.Chrome(service=x)
@@ -57,13 +62,10 @@ driver = webdriver.Chrome(service=x)
 driver.get('https://exoplanets.nasa.gov/discovery/exoplanet-catalog/')
 
 # get title
-print(get_title(driver))
+#print(get_title(driver))
 
-# get exoplanet table
-print(get_table(driver))
-
-
-
+# generate exoplanet dataframe
+print(generate_exoplanet_df(driver))
 
 
 # To keep the browser open fo 50 seconds
