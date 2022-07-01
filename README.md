97385851a0544a07bfb9be6eea11f5243597b7ec
leaf-turner <wesley.shaw.475@my.csun.edu>
2022-07-01 08:26:44 -0700

    got all the pages from the table

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # EXO
 
 # https://stackoverflow.com/questions/52448137/python-selenium-scrape-the-whole-table
+
+# https://selenium-python.readthedocs.io/
--- scrape_exo_data.py
+++ scrape_exo_data.py
@@ -16,13 +16,28 @@ def get_title(driver):
     return driver.title
 
 def get_table(driver):
+    '''
+    1. init lists to hold data for dataframe
+    2. get max page
+    3. set while loop to     while page <= max_page
+    4. create web driver for each page , each time loading into dataframe
+    '''
+    planet_name = []
+    dist_from_earth = []
+    planet_mass = []
+    stellar_magnitude = []
+    discovery_date = []
+
+    # max_pages is the number of pages in the table
+    max_pages = WebDriverWait(driver, 10).until(EC.presence_of_element_located((
+        By.XPATH, '//*[@id="primary_column"]/div[1]/div[2]/div[1]/div/nav/div/span'))).text
+
     try:
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'results')))
+       table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'results')))
     except TimeoutException:
         print('web driver failure')
         driver.quit() 
 
-    table = driver.find_element("id", "results")
     print(table.text)
 
 x=Service('C:\Program Files (x86)\chromedriver.exe')
@@ -36,12 +51,6 @@ print(get_title(driver))
 
 # get exoplanet table
 print(get_table(driver))
-
-
-
-
-
-
 
 
 
